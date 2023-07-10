385aee00ef7d9611be15f119d3d2a24d9693497c
szatmagda <szatmagda@gmail.com>
2023-07-10 20:18:14 +0200

    nowe

--- pages/base_page.py
+++ pages/base_page.py
@@ -16,3 +16,15 @@ class BasePage():
     def get_page_title(self, url):
         self.driver.get(url)
         return self.driver.title
+
+    def assert_element_text(self, driver, xpath, expected_text):
+        """Comparing expected text with observed value from web element
+
+            :param driver: webdriver instance
+            :param xpath: xpath to element with text to be observed
+            :param expected_text: text what we expecting to be found
+            :return: None
+        """
+        element = driver.find_element(by=By.XPATH, value=xpath)
+        element_text = element.text
+        assert expected_text == element_text
